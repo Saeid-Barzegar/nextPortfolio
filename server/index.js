5137d563952b7d2dee08502a35387232a778dce0
Saeid Barzegar <saeidbarzegar@gmail.com>
2022-02-03 22:55:57 +0800

    graphql added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const next = require('next')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -7,8 +9,23 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-
   const server = express()
+
+  const schema = buildSchema(`
+    type Query {
+      hello: String
+    }
+  `);
+
+  const resolver = {
+    hello: () => "Hello GraphQL!"
+  }
+
+  server.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: resolver,
+    graphiql: true,
+  }))
 
   server.all('*', (req, res) => {
     return handle(req, res)
